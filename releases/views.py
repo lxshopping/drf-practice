78b50fde2b2391c91e90907df5667ec4f9cc291f
lixiang <li_xiang111@qq.com>
2026-07-23 16:59:53 +0800

    chore(day01): add HTTP practice harness

--- releases/views.py
+++ releases/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from releases.models import ReleaseOrder
+from releases.serializers import ReleaseOrderSerializer
+
+
+class ReleaseOrderViewSet(viewsets.ModelViewSet):
+    """Day01 的 HTTP 测试入口；练习重点仍是 Serializer。"""
+
+    queryset = ReleaseOrder.objects.all()
+    serializer_class = ReleaseOrderSerializer
